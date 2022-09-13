e65930444dfcf7e8cd2bd9c6fef3cb9f8d78e290
Mohamed Abdelnasser Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-09-13 19:14:37 +0200

    card tested

--- Payment Application/Card/card.c
+++ Payment Application/Card/card.c
@@ -3,7 +3,7 @@
 EN_cardError_t  getCardHolderName(ST_cardData_t*cardData)
 {
     EN_cardError_t result = OK;
-    if(!cardData)
+    if(!cardData->cardHolderName)
     {
         result = WRONG_NAME;
         return result;
@@ -11,7 +11,7 @@ EN_cardError_t  getCardHolderName(ST_cardData_t*cardData)
     else
     {
         int len = strlen(cardData->cardHolderName);
-        if(len < 20 || len >24)
+        if((len < 20) ||  (len > 24))
         {
             result = WRONG_NAME ;
             return result;
@@ -21,3 +21,41 @@ EN_cardError_t  getCardHolderName(ST_cardData_t*cardData)
     return result;
 }
 
+// test code
+int main()
+{
+    ST_cardData_t inputs[4];
+    uint8_t parr[4][25] = {
+       "01234567890123456789",
+        "0123456789012345678901234",
+        "012345678901234",
+        '\0'
+    };
+
+
+    strcpy(inputs[0].cardHolderName , parr[0]);
+    strcpy(inputs[1].cardHolderName , parr[1]);
+    strcpy(inputs[2].cardHolderName , parr[2]);
+    strcpy(inputs[2].cardHolderName , parr[3]);
+
+    EN_cardError_t outputs[4] = {OK};
+
+    outputs[0] = getCardHolderName(inputs[0].cardHolderName);
+    outputs[1] = getCardHolderName(inputs[1].cardHolderName);
+    outputs[2] = getCardHolderName(inputs[2].cardHolderName);
+    outputs[3] = getCardHolderName(inputs[3].cardHolderName);
+    
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        switch (outputs[i])
+        {
+        case WRONG_NAME:
+                printf("WRONG_NAME\n");
+            break;
+        
+        default:
+                printf("OK\n");
+            break;
+        }
+    }
+}

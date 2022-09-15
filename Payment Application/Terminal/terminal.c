c6e9b4d226dc89459ab7dce43f6fba394e1bf8fa
Mohamed Abdelnasser Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-09-15 15:37:37 +0200

    INVALID_MAX_AMOUNT

--- Payment Application/Terminal/terminal.c
+++ Payment Application/Terminal/terminal.c
@@ -54,6 +54,23 @@ EN_terminalError_t getTransactionAmount(ST_terminalData_t * termData)
     return result;    
 }
 
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t * termData)
+{
+    EN_terminalError_t result = EXCEED_MAX_AMOUNT;
+    if( termData->transAmount > termData->maxTransAmount)return result;
+    result = OK;
+    return result;    
+
+}
+
+EN_terminalError_t setMaxAmount(ST_terminalData_t * termData)
+{
+    EN_terminalError_t result = INVALID_MAX_AMOUNT;
+    if( termData->transAmount <= 0.0f)return result;
+    result = OK;
+    return result;    
+}
+
 // test gettransactionDate
 // int main(void)
 // {
@@ -142,6 +159,75 @@ EN_terminalError_t getTransactionAmount(ST_terminalData_t * termData)
 //     return 0;
 // }
 
+// int main(void)
+// {
+//     float testcases[4] = { 500.02f , 65.5f , -65.5f , 0.0f};
+//     ST_terminalData_t inputs[4] = {0};
+//     inputs[0].transAmount = testcases[0];
+//     inputs[1].transAmount = testcases[1];
+//     inputs[2].transAmount = testcases[2];
+//     inputs[3].transAmount = testcases[3];
+
+//     EN_terminalError_t outputs[4] = {OK};
+//     outputs[0] = getTransactionAmount(&inputs[0]);
+//     outputs[1] = getTransactionAmount(&inputs[1]);
+//     outputs[2] = getTransactionAmount(&inputs[2]);
+//     outputs[3] = getTransactionAmount(&inputs[3]);   
+//     for(int i = 0 ; i < 4 ; i++)
+//     {
+//         switch (outputs[i])
+//         {
+//         case INVALID_AMOUNT:
+//                 printf("INVALID_AMOUNT\n");
+//             break;
+        
+//         default:
+//                 printf("OK\n");
+//             break;
+//         }
+//     }
+    
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     float testcases[4] = { 500.02f , 65.5f , 33.5f , 520.0f};
+//     float maxcredit[4] = { 900.00f , 55.5f , 1000.0f , 0.0f};
+
+//     ST_terminalData_t inputs[4] = {0};
+//     inputs[0].transAmount = testcases[0];
+//     inputs[1].transAmount = testcases[1];
+//     inputs[2].transAmount = testcases[2];
+//     inputs[3].transAmount = testcases[3];
+
+//     inputs[0].maxTransAmount = maxcredit[0];
+//     inputs[1].maxTransAmount = maxcredit[1];
+//     inputs[2].maxTransAmount = maxcredit[2];
+//     inputs[3].maxTransAmount = maxcredit[3];
+
+//     EN_terminalError_t outputs[4] = {OK};
+//     outputs[0] = isBelowMaxAmount(&inputs[0]);
+//     outputs[1] = isBelowMaxAmount(&inputs[1]);
+//     outputs[2] = isBelowMaxAmount(&inputs[2]);
+//     outputs[3] = isBelowMaxAmount(&inputs[3]);   
+//     for(int i = 0 ; i < 4 ; i++)
+//     {
+//         switch (outputs[i])
+//         {
+//         case EXCEED_MAX_AMOUNT:
+//                 printf("EXCEED_MAX_AMOUNT\n");
+//             break;
+        
+//         default:
+//                 printf("OK\n");
+//             break;
+//         }
+//     }
+    
+//     return 0;
+// }
+
 int main(void)
 {
     float testcases[4] = { 500.02f , 65.5f , -65.5f , 0.0f};
@@ -152,16 +238,16 @@ int main(void)
     inputs[3].transAmount = testcases[3];
 
     EN_terminalError_t outputs[4] = {OK};
-    outputs[0] = getTransactionAmount(&inputs[0]);
-    outputs[1] = getTransactionAmount(&inputs[1]);
-    outputs[2] = getTransactionAmount(&inputs[2]);
-    outputs[3] = getTransactionAmount(&inputs[3]);   
+    outputs[0] = setMaxAmount(&inputs[0]);
+    outputs[1] = setMaxAmount(&inputs[1]);
+    outputs[2] = setMaxAmount(&inputs[2]);
+    outputs[3] = setMaxAmount(&inputs[3]);   
     for(int i = 0 ; i < 4 ; i++)
     {
         switch (outputs[i])
         {
-        case INVALID_AMOUNT:
-                printf("INVALID_AMOUNT\n");
+        case INVALID_MAX_AMOUNT:
+                printf("INVALID_MAX_AMOUNT\n");
             break;
         
         default:
@@ -172,4 +258,3 @@ int main(void)
     
     return 0;
 }
-

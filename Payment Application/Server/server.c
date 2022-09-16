4105ef18d6fe4c61ad1cfd1182ff63f2f67d987e
Mohamed Abdelnasser Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-09-16 13:34:43 +0200

    server_c_ functions

--- Payment Application/Server/server.c
+++ Payment Application/Server/server.c
@@ -2,6 +2,126 @@
 
 
 ST_accountsDB_t accounts[255] = {0};
-ST_transaction_t trans[255] = {0};
+ST_transaction trans[255] = {0};
+int account_index = __INT32_MAX__;
 
 
+
+EN_serverError_t isValidAccount(ST_cardData_t* cardData)
+{
+	uint8_t* PAN =cardData->primaryAccountNumber;
+	//linear search through AccountsDB
+	int is_exist = 0;
+	int i, j;
+	for (i = 0; i < 255; i++) {
+		//if not empty(no more records)
+		if (accounts[i].primaryAccountNumber[0] != '\0') {
+			int equal = 1;
+			for (j = 0; j < 20; j++) {
+				if (accounts[i].primaryAccountNumber[j] != PAN[j]) {
+					equal = 0;
+					break;
+				}
+			}
+			if (equal) {
+				is_exist = 1;
+				account_index = i;
+				break;
+			}
+		}
+		else {
+			break;
+		}
+	}
+	if (!is_exist) {
+		return ACCOUNT_NOT_FOUND;
+	}
+	return OK_server;
+}
+
+EN_serverError_t isAmountAvailable(ST_terminalData_t* termData)
+{
+	if (accounts[account_index].balance < termData->transAmount) {
+		return LOW_BALANCE;
+	}
+	return OK_server;
+}
+
+EN_serverError_t saveTransaction(ST_transaction* transData)
+{
+	FILE* ptr;
+	ptr = fopen("./INFO.txt", "r");
+	if (!ptr) {
+		return SAVING_FAILED;
+	}
+	int transnum;
+	fscanf(ptr, "%d", &transnum);
+	fclose(ptr);
+	ptr = fopen("./INFO.txt", "w");
+	fprintf(ptr, "%d", transnum+1);
+	fclose(ptr);
+	transData->transactionSequenceNumber = transnum+1;
+	ptr = fopen("./TransactionsDB.txt", "a");
+	if (!ptr) {
+		return SAVING_FAILED;
+	}
+	//-=Transaction=-
+	//	-Card Holder Name:
+	//	-PAN:
+	//	-Expiry Date:
+	//	-Amount:
+	//	-Max Amount:
+	//	-Transaction State:
+	//	-Transaction Sequence Number:
+	fprintf(ptr, "-=Transaction=-\n");
+	fprintf(ptr, "\tCard Holder Name: %s\n", transData->cardHolderData.cardHolderName);
+	fprintf(ptr, "\t-PAN: %s\n", transData->cardHolderData.primaryAccountNumber);
+	fprintf(ptr, "\t-Expiry Date: %s\n", transData->cardHolderData.cardExpirationDate);
+	fprintf(ptr, "\t-Amount: %f\n", transData->terminalData.transAmount);
+	fprintf(ptr, "\t-Max Amount: %f\n", transData->terminalData.maxTransAmount);
+	fprintf(ptr, "\t-Transaction State: ");
+	if (transData->transState == APPROVED) {
+		fprintf(ptr, "APPROVED\n");
+	}
+	else if (transData->transState == DECLINED_INSUFFECIENT_FUND) {
+		fprintf(ptr, "DECLINED_INSUFFECIENT_FUND\n");
+	}
+	else if (transData->transState == DECLINED_STOLEN_CARD) {
+		fprintf(ptr, "DECLINED_STOLEN_CARD\n");
+	}
+	fprintf(ptr, "\t-Transaction Sequence Number: %d\n\n", transData->transactionSequenceNumber);
+	fclose(ptr);
+	return OK_server;
+}
+
+EN_transStat_t recieveTransactionData(ST_transaction *transData)
+{
+	if (isValidAccount(&transData->cardHolderData)==ACCOUNT_NOT_FOUND) {
+		transData->transState = DECLINED_STOLEN_CARD;
+		//if (saveTransaction(transData) == SAVING_FAILED)return INTERNAL_SERVER_ERROR;
+		return DECLINED_STOLEN_CARD;
+	}
+	if (isAmountAvailable(&transData->terminalData)==LOW_BALANCE) {
+		transData->transState = DECLINED_INSUFFECIENT_FUND;
+		//if(saveTransaction(transData) == SAVING_FAILED)return INTERNAL_SERVER_ERROR;
+		return DECLINED_INSUFFECIENT_FUND;
+	}
+	transData->transState = APPROVED;
+	accounts[account_index].balance -= transData->terminalData.transAmount;
+	if (saveTransaction(transData) == SAVING_FAILED) {
+		return INTERNAL_SERVER_ERROR;
+	}
+	return APPROVED;
+}
+
+
+EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction* transData)
+{
+	return OK_server;
+}
+
+int main(void)
+{
+    printf("No error\n");
+    return 0;
+}
--- Payment Application/Server/server.h
+++ Payment Application/Server/server.h
@@ -2,42 +2,43 @@
 #define _SERVER_H_
 #include <stdio.h>
 #include <string.h>
+#include "..\Card\card.h"
+#include "..\Terminal\terminal.h"
 
 typedef unsigned char uint8_t;
-extern ST_accountsDB_t accounts[255] = {0};
-extern ST_transaction_t trans[255] = {0};
+typedef unsigned int uint32_t;
+
+typedef enum EN_transState_t
+{
+    APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, INTERNAL_SERVER_ERROR
+}EN_transStat_t;
+
 typedef struct ST_transaction_t
-{ 
+{
     ST_cardData_t cardHolderData;
-    ST_trminalData_t terminalData;
-    EN_transState_t transState;
+    ST_terminalData_t terminalData;
+    EN_transStat_t transState;
     uint32_t transactionSequenceNumber;
 }ST_transaction;
 
-typedef enum EN_transState_t
-{ 
-    APPROVED DECLINED_INSUFFECIENT FUND,
-    APPROVED, DECLINED_INSUFFECIENT_FUND,
-    DECLINED_STOLEN_CARD,
-    INTERNAL_SERVER_ERROR
-}EN_transStat_t;
-
 typedef enum EN_serverError_t
-{ oK, SAVING_FAILED,
-TRANSACTION_NOT_FOUND,
-ACCOUNT_NOT_FOUND, LOW_BALANCE
+{
+    OK_server, SAVING_FAILED, TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND, LOW_BALANCE
 }EN_serverError_t ;
 
 typedef struct ST_accountsDB_t
-{ 
+{
     float balance;
     uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
 
-EN_transState_t recieveTransactionData(ST_transaction_t *transData);
+EN_transStat_t recieveTransactionData(ST_transaction *transData);
 EN_serverError_t isValidAccount(ST_cardData_t *cardData);
-EN_serverError_t isAmountAvailable(ST_trminalData_t *termData);
-EN_serverError_t saveTransaction(ST_transaction_t *transData);
-EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t *transData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t *termData);
+EN_serverError_t saveTransaction(ST_transaction *transData);
+EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction *transData);
+
+extern ST_accountsDB_t accounts[255] ;
+extern ST_transaction trans[255] ;
 
 #endif
